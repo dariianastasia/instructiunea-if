ef8f0d5fda54cff2da33d4c99ec90c0e0e458197
dariianastasia <71002237+dariianastasia@users.noreply.github.com>
2020-10-16 13:01:04 +0300

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,9 @@
+v1=int(input("Introdu virsta primei persoane"))
+v2=int(input("Introdu virsta persoanei a doua"))
+v3=int(input("Introdu virsta persoanei a treia"))
+if ((v1>18)and(v1<60)):
+    print(v1)
+elif((v2>18)and(v2<60)):
+    print(v2)
+elif((v3>18)and(v3<60)):
+    print(v3)
